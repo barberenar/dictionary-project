e2ffaa50257ddefd5954f544ad1fbd5ac2639d8a
Roberta Barberena <barberenar@gmail.com>
2022-06-09 19:11:37 -0600

    Improved styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,34 +3,55 @@ import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
 setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+
+function handleSubmit(event) {
+  event.prefentDefault();
+  search();
+}
+
+function load() {
+  setLoaded(true);
+  search();
+}
 
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
+  if (loaded) {
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
+      <section>
+        <h1>What word do you want to look up?</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
+        </form>
+        <div className="hint">
+          suggested words: sunset, wine, yoga, plant...
+        </div>
+      </section>
       <br />
-      <Results results={results}/>
+      <Results results={results} />
     </div>
   );
+}
+else {
+  load();
+  return "Loading";
+}
 }
