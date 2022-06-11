ac03c47903aaa46e3a32c0356826d1a678c36a4f
Roberta Barberena <barberenar@gmail.com>
2022-06-11 11:00:04 -0600

    Updated Photos and links

--- src/Photos.js
+++ src/Photos.js
@@ -9,8 +9,8 @@ return (
       {props.photos.map(function (photo, index) {
         return (
           <div className="col-4" key={index}>
-            <a href={photo.src.original} target="_blank" ref="noreferrer">
-              <img src={photo.src.landscape} className="img-fluid" />
+            <a href={photo.src.original} target="_blank" rel="noreferrer">
+              <img src={photo.src.landscape} className="img-fluid" alt="Examples"/>
             </a>
           </div>
         );
