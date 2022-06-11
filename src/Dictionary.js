19a15396cb8570db9fbfd8115b4253056e208977
Roberta Barberena <barberenar@gmail.com>
2022-06-11 10:54:47 -0600

    Style and photos added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,34 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
+
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
 setResults(response.data[0]);
+  }
+
+  function handlePewelsResponse(response) {
+    setPhotos (response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
-  }
+    axios.get(apiUrl).then(handleDictionResponse);
 
+    let pexelsApiKey =
+      "563492ad6f9170000100000174c5df8cd72341c4b3580149861d796f";
+    let pexelsApiUrl =
+      `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+     let headers = { Authorization: `Bearer ${pexelsApiKey}`};
+     axios.get(pexelsApiUrl, { headers: headers}).then(handlePewelsResponse);
+  }
 
 function handleSubmit(event) {
   event.prefentDefault();
@@ -39,14 +52,18 @@ function load() {
       <section>
         <h1>What word do you want to look up?</h1>
         <form onSubmit={handleSubmit}>
-          <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
+          <input
+            type="search"
+            onChange={handleKeywordChange}
+            defaultValue={props.defaultKeyword}
+          />
         </form>
         <div className="hint">
           suggested words: sunset, wine, yoga, plant...
         </div>
       </section>
-      <br />
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
